7552fcc12fdf232b1f194f3e2d15d93fd46fcda0
Izzulmaaali <ibapak984@gmail.com>
2025-01-30 17:32:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function calculate() {
     
     // This will replace the result of any mathematical operation with "I love you"
     if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
-        display.value = 'Semangat Unaaaa';
+        display.value = 'I Love You:)';
     } else {
         try {
             display.value = eval(expression);
