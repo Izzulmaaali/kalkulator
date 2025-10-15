32a966e308ede9bf90bccd977f1971c37bf4988d
huda izzul M <ibapak984@gmail.com>
2025-10-15 18:33:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function calculate() {
     
     // This will replace the result of any mathematical operation with "I love you"
     if (expression.match(/[0-9]+[+\-*/][0-9]+/)) {
-        display.value = 'Terimakasih Sella Cantik';
+        display.value = 'Fuck you';
     } else {
         try {
             display.value = eval(expression);
@@ -23,4 +23,5 @@ function clearDisplay() {
     const display = document.getElementById('display');
     display.value = '';
 }
+
 
